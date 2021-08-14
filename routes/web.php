522f234d949f10cf8a69f5cbe857a18e3b1c63c2
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-08-14 14:47:17 +0800

    add route for token page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/token', [App\Http\Controllers\HomeController::class, 'showToken'])->name('show:token');
+Route::get('/docs', [App\Http\Controllers\HomeController::class, 'showDocs'])->name('show:docs');
