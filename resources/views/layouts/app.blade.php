0e26547fd65d21d3adc7e6a65c09f656219d94ce
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-08-14 14:47:40 +0800

    add nav bar for token page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,18 +46,30 @@
                                 </li>
                             @endif
 
-                            @if (Route::has('register'))
+                            <!-- @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif -->
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/home') }}">{{ __('Home') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('show:docs') }}">{{ __('API Docs') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('show:token') }}">
+                                        {{ __('Token') }}
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        {{ __('Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
