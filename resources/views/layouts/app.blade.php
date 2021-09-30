6de6fe658249f8a062b5354049a71efaaa1c8d0e
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-09-30 14:31:15 +0800

    add navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>JAJAHAN</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    JAJAHAN
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -39,6 +39,12 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('show:data') }}">{{ __('Data') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('show:docs') }}">{{ __('API Docs') }}</a>
+                        </li>
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -52,24 +58,32 @@
                                 </li>
                             @endif -->
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/home') }}">{{ __('Home') }}</a>
+                            @can('isAdmin',auth()->user())
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ __('Manage Data') }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">{{ __('State') }}</a>
+                                    <a class="dropdown-item" href="#">{{ __('District') }}</a>
+                                    <a class="dropdown-item" href="#">{{ __('SubDistrict') }}</a>
+                                    <a class="dropdown-item" href="#">{{ __('Parliament') }}</a>
+                                    <a class="dropdown-item" href="#">{{ __('Dun') }}</a>
+                                </div>
                             </li>
+                            @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('show:docs') }}">{{ __('API Docs') }}</a>
+                                <a class="nav-link" href="{{ route('show:token') }}">{{ __('Token') }}</a>
                             </li>
+                            @endcan
+                            
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('show:token') }}">
-                                        {{ __('Token') }}
-                                    </a>
-                                    <a class="dropdown-item" href="#">
-                                        {{ __('Profile') }}
-                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
