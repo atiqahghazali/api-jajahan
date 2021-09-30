057aeeb4b7264696550df4fc43c17a01a215c7bc
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-09-30 14:30:56 +0800

    add route to show pages

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,25 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/token', [App\Http\Controllers\HomeController::class, 'showToken'])->name('show:token');
-Route::get('/docs', [App\Http\Controllers\HomeController::class, 'showDocs'])->name('show:docs');
+Route::get('/docs', [App\Http\Controllers\DashboardController::class, 'showDocs'])->name('show:docs');
+Route::get('/data', [App\Http\Controllers\DashboardController::class, 'showData'])->name('show:data');
+
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+
+// Route::group([
+//     'prefix' => 'admin',
+//     'namespace' => 'App\Http\Controllers\Admin',
+//     'middleware' => ['is_admin', 'auth']
+// ], function(){
+
+    
+//     Route::group([
+//         'as' => 'admin-user-',
+//     ], function(){
+//         Route::get('/user/create', 'UserController@create')->name('create');
+//         Route::post('/user/store', 'UserController@store')->name('store');
+//         Route::get('/user/list', 'UserController@index')->name('index');
+//         Route::get('/user/profile/{user:uuid}', 'UserController@show')->name('show');
+//     });
+
+// });
