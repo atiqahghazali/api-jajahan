4342f4dba00993a64b03ee6dfcff7e34b47e8ba5
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-09-30 14:32:22 +0800

    display data for user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,26 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-    <br>
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Data') }}</div>
-                <div class="card-body">
+                <div class="card-body pt-5">
                     <table class="table"> 
                         <thead>
                             <tr>
@@ -36,9 +17,9 @@
                         <tbody>
                             @foreach($districts as $district)
                             <tr>
-                                <td>{{$district->name}}</td>
                                 <td>{{$district->state->name}}</td>
-                                <td>{{$district->subdistricts}}</td>
+                                <td>{{$district->name}}</td>
+                                <td>{{$district->subdistricts()->select('name')->get()}}</td>
                             </tr>
                             @endforeach
                         </tbody>
