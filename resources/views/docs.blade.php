b7bbb5a05bf43f318f4872b1970b656e11ef1429
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-10-29 05:57:07 +0800

    add example api

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -8,6 +8,33 @@
                 <div class="card-header">{{ __('Get List of State') }}</div>
                 <div class="card-body">
                     <mark>API Call: </mark>
+                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/states';<br>
+                        $response = Http::withHeaders([<br>
+                            'Accept' => 'application/json',<br>
+                            'Authorization' => 'Bearer [token]'<br>
+                        ])->get($url);
+                    </p>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "id": "1",
+                            "name": "JOHOR",
+                            "name_long": "JOHOR DARUL TAKZIM",
+                            "code2": "JH",
+                            "code3": "JHR",
+                            "capital": "JOHOR BAHRU"
+                        },
+                        {
+                            "id": "2",
+                            "name": "KEDAH",
+                            "name_long": "KEDAH DARUL AMAN",
+                            "code2": "KD",
+                            "code3": "KDH",
+                            "capital": "ALOR SETAR"
+                        }...
+                    ]
+                    </p>
                 </div>
             </div>
         </div>
@@ -19,6 +46,29 @@
                 <div class="card-header">{{ __('Get List of District') }}</div>
                 <div class="card-body">
                     <mark>API Call: </mark>
+                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/districts';<br>
+                        $response = Http::withHeaders([<br>
+                            'Accept' => 'application/json',<br>
+                            'Authorization' => 'Bearer [token]'<br>
+                        ])->get($url);
+                    </p>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "id": "101",
+                            "state_id": "1",
+                            "name": "BATU PAHAT",
+                            "code_3": "BPA"
+                        },
+                        {
+                            "id": "0102",
+                            "state_id": "1",
+                            "name": "JOHOR BAHRU",
+                            "code_3": "JBA"
+                        }...
+                    ]
+                    </p>
                 </div>
             </div>
         </div>
@@ -31,15 +81,27 @@
 
                 <div class="card-body">
                     <mark>API Call: </mark>
-                    <p>$url = 'http://api-jajahan.vigx5njyjw-ewl6nz91o652.p.runcloud.link/api/v1/subdistricts';<br>
+                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/subdistricts';<br>
                         $response = Http::withHeaders([<br>
                             'Accept' => 'application/json',<br>
                             'Authorization' => 'Bearer [token]'<br>
                         ])->get($url);
                     </p>
-                    <mark>Parameter</mark>
-                    <p>name -> Name of Subdistrict</p>
-                    <mark>Example of API calls</mark>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "id": "10101",
+                            "name": "BAGAN",
+                            "district_id": "101"
+                        },
+                        {
+                            "id": "010102",
+                            "name": "CHAAH BAHRU",
+                            "district_id": "0101"
+                        }...
+                    ]
+                    </p>
                 </div>
             </div>
         </div>
@@ -52,13 +114,23 @@
                 <div class="card-body">
                     <mark>API Call: </mark>
                     <p>
-                    $url = 'http://api-jajahan.vigx5njyjw-ewl6nz91o652.p.runcloud.link/api/v1/address';<br>
+                    $url = 'http://jajahan.atiqahghazali.site/api/v1/address';<br>
                     $response = Http::withHeaders([<br>
                         'Accept' => 'application/json',<br>
                         'Authorization' => 'Bearer [token]'<br>
                     ])->get($url,[<br>
                         'search' => $request->subdistrict<br>
                     ]);
+                    </p>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "success": "true",
+                            "message": "Successsfully fetch all users",
+                            "data": "LUBOK, BATU PAHAT, JOHOR"
+                        }
+                    ]
                     </p>
                 </div>
             </div>
@@ -71,6 +143,31 @@
                 <div class="card-header">{{ __('Get List of DUN') }}</div>
                 <div class="card-body">
                     <mark>API Call: </mark>
+                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/duns';<br>
+                        $response = Http::withHeaders([<br>
+                            'Accept' => 'application/json',<br>
+                            'Authorization' => 'Bearer [token]'<br>
+                        ])->get($url);
+                    </p>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "id": "00101",
+                            "parliament_id": "00100",
+                            "kerusi_id": "N.01",
+                            "state_id": "09",
+                            "name": "TITI TINGGI"
+                        },
+                        {
+                            "id": "00102",
+                            "parliament_id": "00100",
+                            "kerusi_id": "N.02",
+                            "state_id": "09",
+                            "name": "BESERI"
+                        }...
+                    ]
+                    </p>
                 </div>
             </div>
         </div>
@@ -82,6 +179,31 @@
                 <div class="card-header">{{ __('Get List of Parliament') }}</div>
                 <div class="card-body">
                     <mark>API Call: </mark>
+                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/parliaments';<br>
+                        $response = Http::withHeaders([<br>
+                            'Accept' => 'application/json',<br>
+                            'Authorization' => 'Bearer [token]'<br>
+                        ])->get($url);
+                    </p>
+                    <mark>Sample Result</mark>
+                    <p>
+                    [
+                        {
+                            "id": "00100",
+                            "parliament_id": "00100",
+                            "chair_id": "P.001",
+                            "state_id": "09",
+                            "name": "PADANG BESAR"
+                        },
+                        {
+                            "id": "00200",
+                            "parliament_id": "00100",
+                            "chair_id": "P.002",
+                            "state_id": "09",
+                            "name": "KANGAR"
+                        }...
+                    ]
+                    </p>
                 </div>
             </div>
         </div>
