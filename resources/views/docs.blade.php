f5aa207d76fe3604c51d7ee43ab76288bdaa755c
atiqahghazali <atiqahghazali09@gmail.com>
2022-01-09 18:46:12 +0800

    update api call

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -7,34 +7,23 @@
             <div class="card">
                 <div class="card-header">{{ __('Get List of State') }}</div>
                 <div class="card-body">
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/states';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "id": "1",
-                            "name": "JOHOR",
-                            "name_long": "JOHOR DARUL TAKZIM",
-                            "code2": "JH",
-                            "code3": "JHR",
-                            "capital": "JOHOR BAHRU"
-                        },
-                        {
-                            "id": "2",
-                            "name": "KEDAH",
-                            "name_long": "KEDAH DARUL AMAN",
-                            "code2": "KD",
-                            "code3": "KDH",
-                            "capital": "ALOR SETAR"
-                        }...
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/states</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/getallstate.png')}}">
                 </div>
             </div>
         </div>
@@ -45,30 +34,23 @@
             <div class="card">
                 <div class="card-header">{{ __('Get List of District') }}</div>
                 <div class="card-body">
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/districts';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "id": "101",
-                            "state_id": "1",
-                            "name": "BATU PAHAT",
-                            "code_3": "BPA"
-                        },
-                        {
-                            "id": "0102",
-                            "state_id": "1",
-                            "name": "JOHOR BAHRU",
-                            "code_3": "JBA"
-                        }...
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/districts</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/getalldistrict.png')}}">
                 </div>
             </div>
         </div>
@@ -81,49 +63,46 @@
 
                 <div class="card-body">
                     <u><b>LIST OF ALL SUBDISTRICTS</b></u><br><br>
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/subdistricts';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "id": "10101",
-                            "name": "BAGAN",
-                            "district_id": "101"
-                        },
-                        {
-                            "id": "010102",
-                            "name": "CHAAH BAHRU",
-                            "district_id": "0101"
-                        }...
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/subdistricts</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
                     <br>
+                    <img src="{{asset('/images/getallsubdistrict.png')}}">
+                    <br><br>
                     <u><b>NAME OF SUBDISTRICT BY ID</b></u><br><br>
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/subdistrict-name-by-id';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url,[
-                            'id' => 10101
-                        ]);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "success": "true",
-                            "message": "Successsfully fetch subdistrict name",
-                            "data": "BAGAN"
-                        }
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/subdistrict-name-by-id</td>
+                        </tr>
+                        <tr>
+                            <td>Params</td>
+                            <td>: 'id'=>'subdistrict_id'</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/subdistrictname.png')}}">
                 </div>
             </div>
         </div>
@@ -134,26 +113,27 @@
             <div class="card">
                 <div class="card-header">{{ __('Get Full Address') }}</div>
                 <div class="card-body">
-                    <p>Example: </p>
-                    <p>
-                    $url = 'http://jajahan.atiqahghazali.site/api/v1/address';<br>
-                    $response = Http::withHeaders([<br>
-                        'Accept' => 'application/json',<br>
-                        'Authorization' => 'Bearer [token]'<br>
-                    ])->get($url,[<br>
-                        'search' => $request->subdistrict<br>
-                    ]);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "success": "true",
-                            "message": "Successsfully fetch all users",
-                            "data": "LUBOK, BATU PAHAT, JOHOR"
-                        }
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/address</td>
+                        </tr>
+                        <tr>
+                            <td>Params</td>
+                            <td>: 'search'=>'subdistrict_id'</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/address.png')}}">
                 </div>
             </div>
         </div>
@@ -164,32 +144,23 @@
             <div class="card">
                 <div class="card-header">{{ __('Get List of DUN') }}</div>
                 <div class="card-body">
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/duns';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "id": "00101",
-                            "parliament_id": "00100",
-                            "kerusi_id": "N.01",
-                            "state_id": "09",
-                            "name": "TITI TINGGI"
-                        },
-                        {
-                            "id": "00102",
-                            "parliament_id": "00100",
-                            "kerusi_id": "N.02",
-                            "state_id": "09",
-                            "name": "BESERI"
-                        }...
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/duns</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/dun.png')}}">
                 </div>
             </div>
         </div>
@@ -200,32 +171,23 @@
             <div class="card">
                 <div class="card-header">{{ __('Get List of Parliament') }}</div>
                 <div class="card-body">
-                    <mark>API Call: </mark>
-                    <p>$url = 'http://jajahan.atiqahghazali.site/api/v1/parliaments';<br>
-                        $response = Http::withHeaders([<br>
-                            'Accept' => 'application/json',<br>
-                            'Authorization' => 'Bearer [token]'<br>
-                        ])->get($url);
-                    </p>
-                    <mark>Sample Result</mark>
-                    <p>
-                    [
-                        {
-                            "id": "00100",
-                            "parliament_id": "00100",
-                            "chair_id": "P.001",
-                            "state_id": "09",
-                            "name": "PADANG BESAR"
-                        },
-                        {
-                            "id": "00200",
-                            "parliament_id": "00100",
-                            "chair_id": "P.002",
-                            "state_id": "09",
-                            "name": "KANGAR"
-                        }...
-                    ]
-                    </p>
+                    <table>
+                        <tr>
+                            <td>Method</td>
+                            <td>: GET</td>
+                        </tr>
+                        <tr>
+                            <td>Authorization</td>
+                            <td>: Bearer < token ></td>
+                        </tr>
+                        <tr>
+                            <td>URL</td>
+                            <td>: http://jajahan.atiqahghazali.site/api/v1/parliaments</td>
+                        </tr>
+                    </table>
+                    <mark>Return Example</mark>
+                    <br>
+                    <img src="{{asset('/images/parliament.png')}}">
                 </div>
             </div>
         </div>
